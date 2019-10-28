5b09a85e4630c4cdfb0691d0c62dfed245ad3ed1
gaelgael <gaelgael5@gmail.com>
2019-10-28 17:43:32 +0100

    add feature mom on rabbitmq with abstraction Black.Beard.Sdk.Brokers.Contracts

--- Src/Black.Beard.Workflows.Outputs.Mom/Outputs/Mom/PushBusActionOutputAction.cs
+++ Src/Black.Beard.Workflows.Outputs.Mom/Outputs/Mom/PushBusActionOutputAction.cs
@@ -1,4 +1,7 @@
-﻿using Bb.Workflows;
+﻿using System;
+using System.Linq;
+using Bb.Brokers;
+using Bb.Workflows;
 using Bb.Workflows.Models;
 using Bb.Workflows.Outputs;
 
@@ -16,19 +19,35 @@ namespace Bb.Workflows.Outputs.Mom
 
         protected override void Prepare_Impl(RunContext ctx)
         {
-
-
+            var actions = ctx.Event.Actions.Where(c => c.Kind == Constants.PushActionName).ToList();
+            this.Items.AddRange(actions);
         }
 
         protected override void Execute_Impl()
         {
-
+            var publisher = GetPublisher();
+            foreach (PushedAction item in this.Items)
+                publisher.Publish(item);
         }
+
+        private IBrokerPublisher GetPublisher()
+        {
+            if (this._publisher == null)
+                this._publisher = this.Brokers.CreatePublisher(this.PublisherName);
+            return this._publisher;
+        }
+
+        public IFactoryBroker Brokers { get; set; }
+
+        public string PublisherName { get; set; }
 
         protected override ITransaction BeginTransaction_Impl()
         {
-            return new PushBusActionTransaction();
+            return new PushBusActionTransaction(GetPublisher());
         }
+
+        private IBrokerPublisher _publisher;
+
     }
 
 }
--- Src/Black.Beard.Workflows.Outputs.Mom/Outputs/Mom/PushBusActionTransaction.cs
+++ Src/Black.Beard.Workflows.Outputs.Mom/Outputs/Mom/PushBusActionTransaction.cs
@@ -1,4 +1,5 @@
-﻿using Bb.Workflows.Outputs;
+﻿using Bb.Brokers;
+using Bb.Workflows.Outputs;
 
 namespace Bb.Workflows.Outputs.Mom
 {
@@ -7,15 +8,24 @@ namespace Bb.Workflows.Outputs.Mom
     public class PushBusActionTransaction : BaseTransaction
     {
 
+        public PushBusActionTransaction(IBrokerPublisher publisher)
+        {
+            this._publisher = publisher;
+            this._transaction = publisher.BeginTransaction();
+        }
+
         protected override void Commit_Impl()
         {
-
+            this._publisher.Commit();
         }
 
         protected override void Rollback_Impl()
         {
-
+            this._publisher.Rollback();
         }
+
+        private readonly IBrokerPublisher _publisher;
+        private readonly Brokers.ITransaction _transaction;
 
     }
 
